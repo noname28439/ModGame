0a74f9b145326c2379c49bc12aea40dfdad2c613
noname28439 <dohle.leon@web.de>
2020-08-17 15:57:44 +0200

    User Login and Registration finished

--- WebPage/main.py
+++ WebPage/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, url_for, session, Markup, redirect, jsonify
 import random
 import dataBaseManager as DBM
+import dataBaseManager
 
 
 app = Flask(__name__)
@@ -18,7 +19,6 @@ DBM.set_pw_salt(SECRET_KEY)
 
 
 naviBar = Markup("""
-
 <ul class="navi">
     <li><a class="aktiv wasonpage" href="/rank/bestPlayers">Scores</a> </li>
     <li><a class="aktiv wasonpage" href="/login">Login</a></li>
@@ -26,8 +26,16 @@ naviBar = Markup("""
     <li><a class="aktiv wasonpage" href="#"></a></li>
     <li><a class="aktiv wasonpage" href="#">Admin Login</a></li>
 </ul>
-
 """)
+
+
+ELEMENT_SCRIPT_BACK = """
+<script>
+    setInterval(function () {
+        window.location=history.back();
+    },1000)
+</script>
+"""
 
 """
 <li><a class="aktiv wasonpage" target="_blank" href="https://www.instagram.com/nicht_leon0/">Instagram</a></li>
@@ -36,6 +44,9 @@ naviBar = Markup("""
     <li><a class="aktiv wasonpage" target="_blank" href="https://www.aes-maintal.de">Schule (mittelmäßig)</a></li>
 """
 
+
+def showShortMessage(text):
+    return Markup("<h1>"+text+"</h1>"+ELEMENT_SCRIPT_BACK)
 
 
 @app.route("/")
@@ -60,10 +71,10 @@ def login():
     if request.method == "POST":
         name = request.form["username"]
         pw = request.form["password"]
-        access = DBM.check_access(name, pw)
+        access = dataBaseManager.check_access(name, pw)
 
         if access == 0:         #Access
-            return Markup("<h1>Wilkommen, "+name+"</h1>")
+            return Markup("<h1>Wilkommen "+name+", du kannst dich jetzt mit deinen Anmeldedaten auch auf den Gameserver anmelden!</h1><!--<br>--><button style=\"font-size: xx-large;\"><a href=\"https://github.com/noname28439/ModGame\">Weitere Informationen findest du auf dem Github des Projektes!</a></button>")
         elif access == 1:       #Wrong PW
             return Markup("<h1>Fehler: Falsches Passwort</h1>")
         elif access == 2:       #Wrong Username
@@ -76,22 +87,27 @@ def login():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        name = request.form["username"]
-        pw = request.form["password"]
-        result = DBM.add_user(name, pw)
 
+        name = request.form["username"]
+        email = request.form["email"]
+        pw = request.form["password"]
+        pw2 = request.form["password2"]
+
+        if not pw==pw2:
+            return showShortMessage("Die eingegebenen Passwörter sind nicht identisch!")
+        if dataBaseManager.user_exists(name):
+            return showShortMessage("Es gibt bereits einen Nutzer mit dem Namen "+name+"!")
+
+
+        result = dataBaseManager.add_user(name, pw, email)
+
+        if result==0:
+            return showShortMessage("Es gibt bereits einen Nutzer mit dem Namen " + name + "!")
+        elif result==1:
+            return showShortMessage("Beim Erstellen des Accounts ist ein Fehler aufgetreten!")
 
     elif request.method == "GET":
         return render_template("register.html", PY_NAVIBAR=naviBar)
-
-
-
-
-
-
-
-
-
 
 
 
