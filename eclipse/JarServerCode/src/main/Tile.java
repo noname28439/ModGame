1f5e43d8459891fa56591f2af53edd489e2b3ccc
noname28439 <dohle.leon@web.de>
2020-08-27 21:25:30 +0200

    coded Tile.java

--- eclipse/JarServerCode/src/main/Tile.java
+++ eclipse/JarServerCode/src/main/Tile.java
@@ -2,4 +2,36 @@ package main;
 
 public class Tile {
 
+	Tile 
+	NORMAL = new Tile(0),
+	HIGHWAY = new Tile(1),
+	WALL = new Tile(2),
+	TRAP = new Tile(3)
+	;
+	
+	
+	
+	
+	private int id;
+	private int key;
+	
+	Tile(int id) {
+		this.id = id;
+		this.key = -1;
+	}
+	
+	public void setID(int id) {
+		this.id = id;
+	}
+	public int getID() {
+		return this.id;
+	}
+	public int getKey() {
+		return this.key;
+	}
+	public void setKey(int key) {
+		this.key = key;
+	}
+	
+	
 }
